ed310cf352ca6ac09d9101014fc931944248590a
Pou Lin <poumason@live.com>
2021-06-19 10:50:21 +0800

    support /start and /leave command to join and leave bot

--- src/bot.js
+++ src/bot.js
@@ -3,6 +3,7 @@
  */
 const TelegramBot = require('node-telegram-bot-api');
 const token = process.env.TELEGRAM_BOT_TOKEN;
+const ChatAPI = require('shopping-radar-sharedcode').db.ChatAPI;
 
 if (!token) {
   throw new Error('BOT_TOKEN must be provided!');
@@ -18,7 +19,47 @@ bot.on('message', (msg) => {
   const chatId = msg.chat.id;
 
   // send a message to the chat acknowledging receipt of their message
+  if (msg.entities && msg.entities.length > 0) {
+    return;
+  }
+
   bot.sendMessage(chatId, 'Received your message');
+});
+
+bot.onText(/\/start/, (msg) => {
+  const chatId = msg.chat.id;
+
+  const api = new ChatAPI();
+  api.getChats(chatId, 'telegram').then((result) => {
+    if (result && result.records.length > 0) {
+      console.log(result.records[0].fields.chat_id);
+      console.log(`The chat id is exist: ${chatId}`);
+      bot.sendMessage(chatId, 'What product do you want to monitor?');
+    } else {
+      api.addChat(chatId, 'telegram', []).then((x) => {
+        console.log(x);
+        bot.sendMessage(chatId, `${msg.chat.first_name}, Welcome.`);
+      }).catch(e => console.error(e));
+    }
+  });
+});
+
+bot.onText(/\/leave/, (msg) => {
+  const chatId = msg.chat.id;
+  const api = new ChatAPI();
+  api.getChats(chatId, 'telegram').then((result) => {
+    if (!result || result.records.length === 0) {
+      console.log(`not find exist chat id: ${chatId}`);
+    } else {
+      const id = result.records[0].id;
+      api.removeChat(id).then((result) => {
+        console.log(result);
+        if (result && result.deleted) {
+          bot.sendMessage(chatId, `Good bye, ${msg.chat.first_name}`);
+        }
+      });
+    }
+  });
 });
 
 bot.on('polling_error', (err) => console.error(err));
